2cb47ba5829c9f7c098e03fd683225281bf10b63
Ruslan <ruslan777mirov@gmail.com>
2020-02-25 10:04:52 +0200

    Finish 51% vote logic

--- contracts/CoTraderDAOWallet.sol
+++ contracts/CoTraderDAOWallet.sol
@@ -16,30 +16,45 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract COTDAOVote is Ownable{
     using SafeMath for uint256;
-    address public COT;
+    ERC20 public COT;
     address[] public voiters;
     mapping(address => address) public mappingVote;
-    address[] public tokens;
 
     function burn(uint256 amount) private {
-      ERC20(COT).transfer(adress(0x0000000000000000000000000000000000000000));
+      COT.transfer(adress(0x0000000000000000000000000000000000000000));
     }
-
 
     function stake(uint256 amount) private {
       // TODO send tokens to stake reserve
     }
 
-    function withdraw() onlyOwner {
+    function withdraw(ERC20[] tokens) onlyOwner {
       // TODO
       // 1/3 to owner address
       // 1/3 burn
       // 1/3 stake
     }
 
-    function addTokenAddress(address _token){
-      tokens.push(_token);
+    function convertTokenToCOT(address _token, uint256 _amount)
+    private
+    returns(uint256 cotAmount){
+      // TODO convert token to COT via Bancor
+      return 0;
     }
+
+    function convertTokenToETH(address _token, uint256 _amount)
+    private
+    returns(uint256 ethAmount){
+      // TODO convert token to ETH for case if token can't be converted to COT
+      return 0;
+    }
+
+    /*
+    ** VOTE LOGIC
+    *
+    *  users can change owner if total balance of COT for all users more then 50%
+    *  of total supply COT
+    */
 
     // subscribe wallet for a vote
     function subscribeForVote()public{
@@ -47,19 +62,36 @@ contract COTDAOVote is Ownable{
     }
 
     // vote for a certain candidate
-    function vote(address _wallet)public{
-        mappingVote[msg.sender] = _wallet;
+    function vote(address _candidate)public{
+       mappingVote[msg.sender] = _candidate;
+    }
+
+    // return total supply - burned balance
+    function calculateCOTSupply() public view returns(uint256){
+      uint256 supply = COT.totalSupply();
+      uint256 burned = COT.balanceOf(adress(0x0000000000000000000000000000000000000000));
+      return supply.sub(burned);
     }
 
     // calculate all vote subscribers
+    // return balance of COT for all voters of current candidate
     function calculateVoters(address _candidate)public view returns(uint256){
-        uint count;
+        uint256 count;
         for(uint i = 0; i<voiters.length; i++){
-            // take into account current vote if this vote compare with current candidate
+            // take into account current vote balance
+            // if this vote compare with current candidate
             if(_candidate == mappingVote[voiters[i]]){
-                count ++;
+                count = count.add(COT.balanceOf(voiters[i]));
             }
         }
         return count;
+    }
+
+    function changeOwner(address _newOwner) public {
+      uint256 totalVoiters = calculateVoters(_newOwner);
+      uint256 totalCOT = calculateCOTSupply();
+      // require 51%
+      require(totalVoiters > totalCOT);
+      super._transferOwnership(_newOwner);
     }
 }
