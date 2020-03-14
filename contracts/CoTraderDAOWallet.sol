85c99f4101ec90ca920f570c5d87cb262b12c3bb
Ruslan <ruslan777mirov@gmail.com>
2020-03-14 07:34:58 +0200

    Txt update

--- contracts/CoTraderDAOWallet.sol
+++ contracts/CoTraderDAOWallet.sol
@@ -2,8 +2,8 @@
 * This contract get 10% from CoTrader managers profit and then distributes assets
 *
 * 50% convert to COT and burn
-* 25% convert to COT and send to stake reserve
-* 25% to owner of this contract (CoTrader team)
+* 10% convert to COT and send to stake reserve
+* 40% to owner of this contract (CoTrader team)
 *
 * NOTE: 51% CoTrader token holders can change owner of this contract
 */
